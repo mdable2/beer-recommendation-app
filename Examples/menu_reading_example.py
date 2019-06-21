a61a6f28079c036f086ee05eb245f13fbc6f525f
Mark Dabler <mdabler@rightpoint.com>
2019-06-20 21:46:12 -0500

    initial set up of repo and outline of examples to do

---
+++ Examples/menu_reading_example.py
@@ -0,0 +1,17 @@
+# Load in image
+
+''' TODO: Set up these connections in a .config file and import '''
+
+# Set up connection to Google Vision API
+
+# Set up connection to GCP Datastore
+
+# Set up connection to Untappd API
+
+# Send image file to Google Vision API and get response
+
+# Sort response into likely beer names based on comparing names with close matches in GCP beer name datastore
+
+# Use the matched beer names to call the Untappd API
+
+# Print out matches by showing name, rating, and ABV
