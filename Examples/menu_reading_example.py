a7a8e2e0ce3e8207ecc16c43a89639e3b09e3f18
Mark Dabler <mdabler@rightpoint.com>
2019-06-22 13:01:51 -0500

    successfully send image to google vision api and retrieve text

--- Examples/menu_reading_example.py
+++ Examples/menu_reading_example.py
@@ -1,8 +1,68 @@
-# Load in image
+import json
+import os
+import io
+from google.cloud import vision
+from google.cloud.vision import types
+from google.oauth2 import service_account
 
-''' TODO: Set up these connections in a .config file and import '''
+config_path = os.path.join("..", "config.json")
 
-# Set up connection to Google Vision API
+google_creds_path = os.path.join("c:/", "Users", "mdabler", "OneDrive - Rightpoint", "Desktop", "beer-recommendation-app-3764c8dfdd8e.json")
+
+google_creds = service_account.Credentials.from_service_account_file(google_creds_path)
+
+with open(config_path) as json_data_file:
+    config_data = json.load(json_data_file)
+
+
+''' Set up connection to Google Vision API '''
+# Instantiates a client
+client = vision.ImageAnnotatorClient(credentials=google_creds)
+
+# The name of the image file to annotate
+file_name = os.path.join("..", "Resources", "Images", "beer_menu_1.png")
+
+# def detect_text(path):
+#     """Detects text in the file."""
+#     from google.cloud import vision
+#     client = vision.ImageAnnotatorClient()
+
+#     with io.open(path, 'rb') as image_file:
+#         content = image_file.read()
+
+#     image = vision.types.Image(content=content)
+
+#     response = client.text_detection(image=image)
+#     texts = response.text_annotations
+#     print('Texts:')
+
+#     for text in texts:
+#         print('\n"{}"'.format(text.description))
+
+#         vertices = (['({},{})'.format(vertex.x, vertex.y)
+#                     for vertex in text.bounding_poly.vertices])
+
+#         print('bounds: {}'.format(','.join(vertices)))
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+# Performs label detection on the image file
+response = client.text_detection(image=image)
+texts = response.text_annotations
+
+print('Texts:')
+
+for text in texts:
+    print('\n"{}"'.format(text.description))
+
+    vertices = (['({},{})'.format(vertex.x, vertex.y)
+                for vertex in text.bounding_poly.vertices])
+
+    print('bounds: {}'.format(','.join(vertices)))
 
 # Set up connection to GCP Datastore
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Beer-Recommendator-App
 Take a picture of the beer menu at a restaurant and get back the top choice based on your preferences!
+
+### How To's
+
+1. [Setting Up Virtual Environment For Python](https://cloud.google.com/python/setup)
+2. [Simple "Hello World" Using Google Cloud Vision](https://cloud.google.com/vision/docs/quickstart-client-libraries#client-libraries-install-python)
